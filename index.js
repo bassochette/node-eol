9d9076d8c58541d34fefe1f31f793b6b39dccfa9
Julien Prugne <julien.prugne@osedea.com>
2015-06-15 22:35:44 -0400

    0.1.2 fix \r\n finding

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-module.exports = function findEolChar(data){
-    if(typeof data === 'string'){
+module.exports = function findEolChar(data) {
+    if (typeof data === 'string') {
         var r = data.split("\r")
-            var n = data.split("\n")
-            if( r.length > 1 && n.length === 1){
-                return "\r"
-            } else if ( r.length === 1 && n.length > 1){
-                return "\n"
-            } else if ( r.length > 1 && r[1] === "\n" ){
-                return "\r\n"    
-            } else {
-                return undefined    
-            }
+        var n = data.split("\n")
+        if (r.length > 1 && n.length === 1) {
+            return "\r"
+        } else if (r.length === 1 && n.length > 1) {
+            return "\n"
+        } else if (r.length > 1 && n.length > 1 ){
+            return "\r\n"
+        } else {
+            return undefined
+        }
     } else {
-        throw new Error('Argument must be a string')   
-    }    
+        throw new Error('Argument must be a string')
+    }
 }
 
--- test/test.js
+++ test/test.js
@@ -3,13 +3,16 @@ var eol = require('../index.js')
 
 //assert.throws(eol([]), Error, "throw error if no string in agrs")
 
-var txt = "toto\ttiti\r"
+var txt = "toto\ttiti\rtutu"
 assert.equal(eol(txt), "\r", "Find \\r eol")
-txt = "toto\ttiti\n"
+console.log("\\r char eol ok")
+txt = "toto\ttiti\ntutu"
 assert.equal(eol(txt), "\n", "find \\n eol")
-txt = "toto\ttiti\r\n"
+console.log("\\n char eol ok")
+txt = "toto\ttiti\r\ntutu"
 assert.equal(eol(txt), "\r\n", "find \\r\\n eol")
-txt = "toto\ttiti"
+console.log("\\r\\n char ok")
+txt = "toto\ttiti\ttutu"
 assert.equal(eol(txt), undefined, "no eol in text")
 
 console.log('All tests passed!!!')
